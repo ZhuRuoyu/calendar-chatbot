b083ada59b117a253cc828ad365ed3cd455d5a81
ZhuRuoyu <ruoyuzhu812@gmail.com>
2023-07-10 09:29:27 -0400

    add pipline to customized model

--- customized_model/customNER/code-for-ner.py
+++ customized_model/customNER/code-for-ner.py
@@ -2,7 +2,8 @@
 
 pip install -U pip setuptools wheel
 pip install -U spacy
-python -m spacy download en_core_web_sm
+#python -m spacy download en_core_web_sm 
+python -m spacy download en_core_web_lg
 
 #A config.cfg file will appear in your working directory.
 
@@ -10,7 +11,7 @@ python -m spacy init fill-config base_config.cfg config.cfg
 
 
 #brgin training After training is complete, the resulting model will appear in a new folder called output.
-python -m spacy train config.cfg — output ./output
-
+#python -m spacy train config.cfg — output ./output
+python -m spacy train config.cfg --output ./output
 
 
